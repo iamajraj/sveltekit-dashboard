512801b109fab8fed506fd85f4d57195667ba392
iamajraj <akmalraj07@gmail.com>
2024-06-02 17:33:03 +0600

    _update: some code refactor

--- src/routes/datatable/+page.server.ts
+++ src/routes/datatable/+page.server.ts
@@ -2,15 +2,8 @@ import { getDatatableUsers } from '$lib/api/datatable';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params }) => {
-	try {
-		const users = await getDatatableUsers();
-		return {
-			users
-		};
-	} catch (error) {
-		console.error('Error fetching data:', error);
-		return {
-			users: []
-		};
-	}
+	const users = await getDatatableUsers();
+	return {
+		users
+	};
 };
